8405d120eef125d0f44e6f54d7be334997604e7a
chengwei0815 <chengwei0815@gmail.com>
2023-05-06 22:20:33 -0400

    create comics.json via MongoDB

--- data/data.js
+++ data/data.js
@@ -1,8 +1,9 @@
 const { v4: uuid } = require('uuid');
 
+
 module.exports  =  [
     {
-        _id: uuid(),
+
         title: "Fun Home: A Family Tragicomic",
         author: "Alison Bechdel",
         rating: 5,
@@ -13,7 +14,7 @@ module.exports  =  [
         synopsis: "In this graphic memoir, Alison Bechdel charts her fraught relationship with her late father.\nDistant and exacting, Bruce Bechdel was an English teacher and director of the town funeral home, which Alison and her family referred to as the Fun Home. It was not until college that Alison, who had recently come out as a lesbian, discovered that her father was also gay. A few weeks after this revelation, he was dead, leaving a legacy of mystery for his daughter to resolve.",
     },
     {
-        _id: uuid(),
+
         title: "Watchmen",
         author: "Alan Moore",
         rating: 5,
@@ -24,7 +25,7 @@ module.exports  =  [
         synopsis: "This Hugo Award-winning graphic novel chronicles the fall from grace of a group of super-heroes plagued by all-too-human failings. Along the way, the concept of the super-hero is dissected as the heroes are stalked by an unknown assassin.\nOne of the most influential graphic novels of all time and a perennial best-seller, Watchmen has been studied on college campuses across the nation and is considered a gateway \"title\", leading readers to other graphic novels such as V for Vendetta, Batman: The Dark Knight Returns, and The Sandman series.",
     },
     {
-        _id: uuid(),
+
         title: "Hunter X Hunter Vol. 1",
         author: "Yoshihiro Togashi",
         rating: 5,
@@ -35,7 +36,7 @@ module.exports  =  [
         synopsis: "Hunters are a special breed, dedicated to tracking down treasures, magical beasts, and even other people. But such pursuits require a license, and less than one in a hundred thousand can pass the grueling qualification exam. Those who do pass gain access to restricted areas, amazing stores of information, and the right to call themselves Hunters.\nGon might be a country boy, but he has high aspirations. Despite his Aunt Mito's protests, Gon decides to follow in his father's footsteps and become a legendary Hunter. The Hunter hopefuls begin their journey by storm-tossed ship, where Gon meets Leorio and Kurapika, the only other applicants who aren't devastated by bouts of seasickness.\nHaving survived the terrors of the high seas, Gon and his companions now have to prove their worth in a variety of tests in order to find the elusive Exam Hall. And once they get there, will they ever leave alive...?",
     },
     {
-        _id: uuid(),
+
         title: "Lumberjanes Vol. 1",
         author: "Noelle Stevenson",
         rating: 4,
@@ -46,7 +47,7 @@ module.exports  =  [
         synopsis: "At Miss Qiunzilla Thiskwin Penniquiqul Thistle Crumpet's camp for hard-core lady-types, things are not what they seem. Three-eyed foxes. Secret caves. Anagrams. Luckily, Jo, April, Mal, Molly, and Ripley are five rad, butt-kicking best pals determined to have an awesome summer together... And they're not gonna let a magical quest or an array of supernatural critters get in their way! The mystery keeps getting bigger, and it all begins here.",
     },
     {
-        _id: uuid(),
+
         title: "One Piece, Vol. 1: Romance Dawn",
         author: "Eiichiro Oda",
         rating: 5,
@@ -57,7 +58,7 @@ module.exports  =  [
         synopsis: "A new shonen sensation in Japan, this series features Monkey D. Luffy, whose main ambition is to become a pirate. Eating the Gum-Gum Fruit gives him strange powers but also invokes the fruit's curse: anybody who consumes it can never learn to swim. Nevertheless, Monkey and his crewmate Roronoa Zoro, master of the three-sword fighting style, sail the Seven Seas of swashbuckling adventure in search of the elusive treasure \"One Piece.\"",
     },
     {
-        _id: uuid(),
+
         title: "Wake: The Hidden History of Women-Led Slave Revolts",
         author: "Rebecca Hall",
         rating: 4,
@@ -68,7 +69,7 @@ module.exports  =  [
         synopsis: "Part graphic novel, part memoir, Wake is an imaginative tour-de-force that tells the story of women-led slave revolts and chronicles scholar Rebecca Hall’s efforts to uncover the truth about these women warriors who, until now, have been left off the historical record.\nWomen warriors planned and led slave revolts on slave ships during the Middle Passage. They fought their enslavers throughout the Americas. And then they were erased from history.\nWake tells the story of Dr. Rebecca Hall, a historian, granddaughter of slaves, and a woman haunted by the legacy of slavery. The accepted history of slave revolts has always told her that enslaved women took a back seat. But Rebecca decides to look deeper, and her journey takes her through old court records, slave ship captain’s logs, crumbling correspondence, and even the forensic evidence from the bones of enslaved women from the “negro burying ground” uncovered in Manhattan. She finds women warriors everywhere.\nUsing in-depth archival research and a measured use of historical imagination, Rebecca constructs the likely pasts of Adono and Alele, women rebels who fought for freedom during the Middle Passage, as well as the stories of women who led slave revolts in Colonial New York. We also follow Rebecca’s own story as the legacy of slavery shapes life, both during her time as a successful attorney and later as a historian seeking the past that haunts her.\nIllustrated beautifully in black and white, Wake will take its place alongside classics of the graphic novel \"genre\", like Marjane Satrapi’s Persepolis and Art Spiegelman’s Maus. The story of both a personal and national legacy, it is a powerful reminder that while the past is gone, we still live in its wake.",
     },
     {
-        _id: uuid(),
+
         title: "Black Panther: A Nation Under Our Feet Book 1",
         author: "Ta-Nehisi Coates",
         rating: 3,
@@ -79,7 +80,7 @@ module.exports  =  [
         synopsis: "A new era begins for the Black Panther! MacArthur Genius and National Book Award-winning writer Ta-Nehisi Coates (Between the World and Me) takes the helm, confronting T'Challa with a dramatic upheaval in Wakanda that will make leading the African nation tougher than ever before. When a superhuman terrorist group that calls itself The People sparks a violent uprising, the land famed for its incredible technology and proud warrior traditions will be thrown into turmoil. If Wakanda is to survive, it must adapt--but can its monarch, one in a long line of Black Panthers, survive the necessary change? Heavy lies the head that wears the cowl!",
     },
     {
-        _id: uuid(),
+
         title: "The Walking Dead, Vol. 1: Days Gone Bye",
         author: "Robert Kirkman",
         rating: 4,
@@ -90,7 +91,7 @@ module.exports  =  [
         synopsis: "The world we knew is gone. The world of commerce and frivolous necessity has been replaced by a world of survival and responsibility. An epidemic of apocalyptic proportions has swept the globe, causing the dead to rise and feed on the living. In a matter of months society has crumbled: no government, no grocery stores, no mail delivery, no cable TV. In a world ruled by the dead, the survivors are forced to finally start living.",
     },
     {
-        _id: uuid(),
+
         title: "March: Book One",
         author: "John Lewis",
         rating: 5,
@@ -101,7 +102,7 @@ module.exports  =  [
         synopsis: "March is a vivid first-hand account of John Lewis’ lifelong struggle for civil and human rights, meditating in the modern age on the distance traveled since the days of Jim Crow and segregation. Rooted in Lewis’ personal story, it also reflects on the highs and lows of the broader civil rights movement.\nBook One spans John Lewis’ youth in rural Alabama, his life-changing meeting with Martin Luther King, Jr., the birth of the Nashville Student Movement, and their battle to tear down segregation through nonviolent lunch counter sit-ins, building to a stunning climax on the steps of City Hall.\nMany years ago, John Lewis and other student activists drew inspiration from the 1950s comic book \"Martin Luther King and the Montgomery Story.\" Now, his own comics bring those days to life for a new audience, testifying to a movement whose echoes will be heard for generations.",
     },
     {
-        _id: uuid(),
+
         title: "Batman: The Dark Knight Returns",
         author: "Frank Miller",
         rating: 3,
@@ -112,7 +113,7 @@ module.exports  =  [
         synopsis: "This masterpiece of modern comics storytelling brings to vivid life a dark world and an even darker man. Together with inker Klaus Janson and colorist Lynn Varley, writer/artist Frank Miller completely reinvents the legend of Batman in his saga of a near-future Gotham City gone to rot, ten years after the Dark Knight's retirement.\nCrime runs rampant in the streets, and the man who was Batman is still tortured by the memories of his parents' murders. As civil society crumbles around him, Bruce Wayne's long-suppressed vigilante side finally breaks free of its self-imposed shackles.\nThe Dark Knight returns in a blaze of fury, taking on a whole new generation of criminals and matching their level of violence. He is soon joined by this generation's Robin—a girl named Carrie Kelley, who proves to be just as invaluable as her predecessors.\nBut can Batman and Robin deal with the threat posed by their deadliest enemies, after years of incarceration have made them into perfect psychopaths? And more important, can anyone survive the coming fallout of an undeclared war between the superpowers—or a clash of what were once the world's greatest superheroes?\nOver fifteen years after its debut, Batman: The Dark Knight Returns remains an undisputed classic and one of the most influential stories ever told in the comics medium.",
     },
     {
-        _id: uuid(),
+
         title: "Queer: A Graphic History",
         author: "Meg-John Barker",
         rating: 4,
@@ -123,7 +124,7 @@ module.exports  =  [
         synopsis: "Activist-academic Meg-John Barker and cartoonist Julia Scheele illuminate the histories of queer thought and LGBTQ+ action in this groundbreaking non-fiction graphic novel.\nFrom identity politics and gender roles to privilege and exclusion, Queer explores how we came to view sex, gender and sexuality in the ways that we do; how these ideas get tangled up with our culture and our understanding of biology, psychology and sexology; and how these views have been disputed and challenged.\nAlong the way we look at key landmarks which shift our perspective of what’s ‘normal’ – Alfred Kinsey’s view of sexuality as a spectrum, Judith Butler’s view of gendered behaviour as a performance, the play Wicked, or moments in Casino Royale when we’re invited to view James Bond with the kind of desiring gaze usually directed at female bodies in mainstream media.\nPresented in a brilliantly engaging and witty style, this is a unique portrait of the universe of queer thinking.",
     },
     {
-        _id: uuid(),
+   
         title: "Parable of the Sower",
         author: "Octavia E. Butler",
         rating: 4,
